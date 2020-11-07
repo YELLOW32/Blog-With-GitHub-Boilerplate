21eced2760680b842a859ebf11e900da92412916
YELLOW32 <74090052+YELLOW32@users.noreply.github.com>
2020-11-07 20:14:19 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,25 +19,25 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "YELLOW 32"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "YELLOW"
+email = "2044127201@qq.com"
+author_homepage = "https://www.github.com/YELLOW32"
+description = "Everything you do,they are all yellow"
+key_words = ['YELLOW32', 'YELLOW', '32', 'blog']
 language = 'zh-CN'
 external_links = [
     {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
+        "name": "YELLOW32",
+        "url": "https://github.com/YELLOW32",
+        "brief": "🏄‍ Go My Own FUCKING Way."
     },
     {
         "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "url": "https://www.github.com/YELLOW32",
+        "brief": "YELLOW 32的主页。"
     }
 ]
 nav = [
@@ -61,17 +61,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/YELLOW32",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/5602436052/",
         "icon": "gi gi-weibo"
     }
 ]
