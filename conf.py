4bd9850f18da2db9e4aa156f85c6c1c595369d59
YELLOW32 <74090052+YELLOW32@users.noreply.github.com>
2020-11-07 20:24:33 +0800

    qqqq
    qq

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ external_links = [
         "brief": "🏄‍ Go My Own FUCKING Way."
     },
     {
-        "name": "三無計劃",
+        "name": "三無 計劃",
         "url": "https://www.github.com/YELLOW32",
         "brief": "YELLOW 32的主页。"
     }
